8d95514833b83a4782f1e002094a429356a7dca0
xuqing <2750493427@qq.com>
2024-10-17 01:36:05 +0800

    test.cpp文件创建，未写完

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void sort(int arr[], int size) {
+  // 还未实现
+}
+int main() {
+  int arr[] = {1, 3, 2, 6, 4, 8};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  sort(arr, size);
+  return 0;
+}
